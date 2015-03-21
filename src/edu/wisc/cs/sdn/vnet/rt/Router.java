4fa3f7eb17d8db515f377afd315795dea225a20c
Corbin Schwalm <corbin.schwalm@twinlakesprogramming.com>
2015-03-21 17:30:35 -0500

    fix

--- src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -191,9 +191,9 @@ public class Router extends Device
     	
     	/* Flip Source/Dest To Reply Back */
     	ether.setSourceMACAddress(iface.getMacAddress().toBytes());
-    	//RouteEntry routeMapping = this.routeTable.lookup(failedIpPacket.getSourceAddress());
-    	//ArpEntry dstAddress = this.arpCache.lookup(routeMapping.getDestinationAddress());
-    	ether.setDestinationMACAddress(failedEtherPacket.getSourceMACAddress());
+    	RouteEntry routeMapping = this.routeTable.lookup(failedIpPacket.getSourceAddress());
+    	ArpEntry dstAddress = this.arpCache.lookup(routeMapping.getDestinationAddress());
+    	ether.setDestinationMACAddress(dstAddress.getMac().toBytes());
 
     	IPv4 ip = new IPv4();
     	ip.setTtl(new Integer(64).byteValue());
